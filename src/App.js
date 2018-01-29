83e8e906b98ddc9ecfcfb7beb82381ed9af384e9
meganmfisher <meganmichellefisher@gmail.com>
2018-01-28 19:41:11 -0700

    switcharoo

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ class App extends Component {
   constructor() {
     super() 
 
-    console.log('Constructor: Default state time')
-
     this.state = {
       count: 0
     }
@@ -22,56 +20,10 @@ class App extends Component {
     })
   }
 
-  componentWillMount() {
-    console.log('ComponentWillMount: Component is about to mount')
-  }
-  //There is no component to play with yet, so you can’t do anything involving the DOM. Nothing has changed since your component’s constructor was called, which is where you should be setting up your component’s default configuration. Most Common Use Case: App configuration in your root component. Can call setState but don’t. Use default state instead.
-
-  componentDidMount() {
-    console.log('ComponentDidMount: Component just mounted')
-  }
-  // Doing AJAX in componentDidMount will guarantee that there’s a component to update. Most Common Use Case: Starting AJAX calls to load in data for your component. Can call setState.
-
-  shouldComponentUpdate(newProps, newState) {
-    // console.log(newProps, 'newProps') //any new props from parent
-    // console.log(newState, 'newState') //what new state is going to be
-    console.log("shouldComponentUpdate: Should component update?");
-
-    if (newState.count < 5) {
-      console.log("shouldComponentUpdate: Component should update!");
-      return true;
-    } else {
-      console.log("shouldComponentUpdate: Component should not update!");
-      // React.unmountComponentAtNode(document.getElementById('root'));
-      return false;
-    }
-  }
-  //Should always return a boolean — an answer to the question, “should I re-render?” The default is that it always returns true. Awesome place to improve performance. Most Common Use Case: Controlling exactly when your component will re-render. Cannot call setState.
-
-  componentWillUpdate(newProps, newState) {
-    // console.log(newProps, 'newProps') //any new props from parent
-    // console.log(newState, 'newState') //what new state is going to be
-    console.log('ComponentWillUpdate: Component is about to update')
-  }
-  //Used instead of componentWillReceiveProps on a component that also has shouldComponentUpdate. Cannot call setState.
-
-  componentDidUpdate(currentProps, currentState) {
-    // console.log(currentProps, 'currentProps') //any current props
-    // console.log(currentState, 'currentState') //what state currently is
-    console.log('ComponentDidUpdate: Component just updated')
-  }
-  //Most Common Use Case: Updating the DOM in response to prop or state changes. Can call setState.
-
-  componentWillUnmount() {
-    console.log('ComponentWillUnmount: Component is about to be removed from the DOM')
-  }
-  //Most Common Use Case: Cleaning up any leftover debris from your component. Cannot call setState.
-
 
 
 
   render() {
-    console.log('Rendering App.js')
     return (
       <div className="App">
         <header className="App-header">
